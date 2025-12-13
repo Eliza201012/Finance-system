7121253ab2656b3129f4dbbeb779306dfca9f2d5
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-12-13 18:00:01 +0200

    прописані url шляхи для функцій (logout, signup, profile, login)

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("signup/", views.signup, name="signup"),
+    path("profile/", views.profile, name="profile"),
+    path("login/", views.custom_login, name="custom_login")
+]
