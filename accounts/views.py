9e70dbe036b74fc22898b69e9a75a89a18ec9e98
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-12-13 13:57:05 +0200

    створені функції для аутентифікації (signup, profile, custom_login)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .forms import SignUpForm, UpdateUserForm, UpdateProfileForm
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "You have successfully registered and logged in!:)")
+            return redirect("accounts:profile")
+    else:
+        form = SignUpForm()
+    return render(request, "accounts/signup.html", {"form" : form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your profile is update successfully!")
+            return redirect("accounts:profile")
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, "accounts/profile.html", {"user_form" : user_form, "profile_form" : profile_form})
+
+def custom_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "You successfully login :)")
+            return redirect("accounts:profile")
+    return render(request, "accounts/custom_login.html", {"form" : form})
